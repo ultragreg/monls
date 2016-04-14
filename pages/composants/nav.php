71ea2cf0183c9496491230c365265db5009c578d
Lionel Capdecomme <capdecomme.l@mipih.fr>
2016-04-14 13:51:09 +0200

    Correction mineure sur le titre

--- pages/composants/nav.php
+++ pages/composants/nav.php
@@ -19,7 +19,7 @@ include_once 'objects/jeu.php';
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a href="index.php">                
+                <a href="index.php">
                     <img src="../dist/css/logo.png" alt="logo"/>
                 </a>
                 <?php 
